d1f0b60f571eb6b9674f8322a8ee8e424aabcec3
Lizaveta Maslova <elizaveta-maslova-2000@mail.ru>
2024-08-24 23:19:08 +0300

    на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const renderCards = (cards) => {
+  const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+  cards.forEach(({ name, link }) => {
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardElement = cardTemplate.cloneNode(true).querySelector(".card");
 
-// @todo: Функция создания карточки
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+    const likeButton = cardElement.querySelector(".card__like-button");
 
-// @todo: Функция удаления карточки
+    cardImage.src = link;
+    cardImage.alt = name;
+    cardTitle.textContent = name;
 
-// @todo: Вывести карточки на страницу
+    deleteButton.addEventListener("click", () => cardElement.remove());
+
+    likeButton.addEventListener("click", function (evt) {
+      evt.target.classList.toggle("card__like-button_is-active");
+    });
+
+    placesList.append(cardElement);
+  });
+};
+
+renderCards(initialCards);
