46dcc27ce3016d901d85b07c298054ff86b680e6
Lizaveta Maslova <elizaveta-maslova-2000@mail.ru>
2024-08-25 12:23:04 +0300

    на ревью2

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,31 @@
-const renderCards = (cards) => {
+const handleCardDelete = (cardElement) => {
+  cardElement.remove();
+};
+
+const createCard = (cardData, handleDelete) => {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener("click", () => handleDelete(cardElement));
+
+  return cardElement;
+};
+
+const renderCards = (cardElements, handleDelete) => {
   const placesList = document.querySelector(".places__list");
 
-  cards.forEach(({ name, link }) => {
-    const cardTemplate = document.querySelector("#card-template").content;
-    const cardElement = cardTemplate.cloneNode(true).querySelector(".card");
-
-    const cardImage = cardElement.querySelector(".card__image");
-    const cardTitle = cardElement.querySelector(".card__title");
-    const deleteButton = cardElement.querySelector(".card__delete-button");
-    const likeButton = cardElement.querySelector(".card__like-button");
-
-    cardImage.src = link;
-    cardImage.alt = name;
-    cardTitle.textContent = name;
-
-    deleteButton.addEventListener("click", () => cardElement.remove());
-
-    likeButton.addEventListener("click", function (evt) {
-      evt.target.classList.toggle("card__like-button_is-active");
-    });
-
+  cardElements.forEach((cardData) => {
+    const cardElement = createCard(cardData, handleDelete);
     placesList.append(cardElement);
   });
 };
 
-renderCards(initialCards);
+renderCards(initialCards, handleCardDelete);
